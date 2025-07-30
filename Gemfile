1f4e005f53ce7d5333f9622820250de68c5845eb
Arseniy <ashemagonov@royalsw.me>
2025-07-30 18:28:03 +0500

    init-commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "grape", "~> 2.4"
+
+gem "rubocop", "~> 1.79"
+
+gem "telegram-bot-ruby", "~> 2.4"
---
+++ app/app.rb
@@ -0,0 +1,31 @@
+module Twitter
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'twitter'
+    format :json
+    prefix :api
+
+    helpers do
+      def current_user
+        @current_user ||= User.authorize!(env)
+      end
+
+      def authenticate!
+        error!('401 Unauthorized', 401) unless current_user
+      end
+    end
+
+    resource :info do
+      desc 'Create a status.'
+      params do
+        requires :status, type: String, desc: 'Your status.'
+      end
+      post do
+        authenticate!
+        Status.create!({
+          user: current_user,
+          text: params[:status]
+        })
+      end
+    end
+  end
+end
